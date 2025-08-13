77daf5e46e9e82c8414f35b70da7336a88f4eceb
ayush-that <ayush1337@hotmail.com>
2025-08-13 17:21:24 +0530

    remove cringe text

--- components/Custom404Component.tsx
+++ components/Custom404Component.tsx
@@ -1,21 +1,9 @@
 "use client";
 
 import React, { Suspense } from "react";
-import { useSearchParams } from "next/navigation";
 
 const SearchParamsContent: React.FC = () => {
-  const searchParams = useSearchParams();
-  const referrer = searchParams?.get("ref");
-
-  return (
-    <>
-      {referrer ? (
-        <p>Oops! It seems like there was an issue coming from {referrer}.</p>
-      ) : (
-        <p>It looks like nothing was found at this location.</p>
-      )}
-    </>
-  );
+  return <></>;
 };
 
 const Custom404Component: React.FC = () => {
