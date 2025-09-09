f250974a88176dd37f3a9728ab280795c9584205
ayush-that <ayush1337@hotmail.com>
2025-09-09 13:13:38 +0530

    remove bs

--- app/api/health/route.ts
+++ app/api/health/route.ts
@@ -1,19 +1,10 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  try {
-    return new NextResponse("OK", {
-      status: 200,
-      headers: {
-        "Content-Type": "text/plain",
-      },
-    });
-  } catch (error) {
-    return new NextResponse("ERROR", {
-      status: 500,
-      headers: {
-        "Content-Type": "text/plain",
-      },
-    });
-  }
+  return new NextResponse("OK", {
+    status: 200,
+    headers: {
+      "Content-Type": "text/plain",
+    },
+  });
 }
